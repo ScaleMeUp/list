6120124df957c7559844572662e35fac3da497bf
zelvad <vladislav.zelenyk@scalemeup.com>
2024-11-10 19:03:39 +0200

    remove dist folder from gitignore

---
+++ dist/list.mjs
@@ -0,0 +1,469 @@
+(function(){"use strict";try{if(typeof document<"u"){var t=document.createElement("style");t.appendChild(document.createTextNode('.cdx-list{margin:0;padding-left:40px;outline:none}.cdx-list__item{padding:5.5px 0 5.5px 5px;line-height:1.6em}.cdx-list--compact .cdx-list__item{padding-top:0!important;padding-bottom:0!important}.cdx-list--ordered{list-style:decimal}.cdx-list--unordered{list-style-type:disc}.cdx-list--circled{list-style-type:circle}.cdx-list--squared{list-style-type:square}.cdx-list--stared{list-style-type:"★"}.cdx-list--dashed{list-style-type:"-"}.cdx-list--arrowed{list-style-type:"→"}.cdx-list-settings{display:flex}.cdx-list-settings .cdx-settings-button{width:50%}[data-item-name=normal] .ce-popover-item__icon,[data-item-name=compact] .ce-popover-item__icon{width:1px;overflow:hidden;opacity:0}')),document.head.appendChild(t)}}catch(e){console.error("vite-plugin-css-injected-by-js",e)}})();
+const a = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><line x1="9" x2="19" y1="7" y2="7" stroke="currentColor" stroke-linecap="round" stroke-width="2"/><line x1="9" x2="19" y1="12" y2="12" stroke="currentColor" stroke-linecap="round" stroke-width="2"/><line x1="9" x2="19" y1="17" y2="17" stroke="currentColor" stroke-linecap="round" stroke-width="2"/><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5.00001 17H4.99002"/><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5.00001 12H4.99002"/><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5.00001 7H4.99002"/></svg>', l = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13.6303 7.09091V18H11.3239V9.28018H11.2599L8.76172 10.8462V8.80078L11.4624 7.09091H13.6303Z" fill="currentColor"/>
+</svg>`, o = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.5653 15.7955C11.8778 15.7955 11.25 15.6278 10.6818 15.2926C10.1136 14.9517 9.65909 14.4972 9.31818 13.929C8.98295 13.3608 8.81534 12.733 8.81534 12.0455C8.81534 11.3523 8.98295 10.7244 9.31818 10.1619C9.65909 9.59375 10.1136 9.14205 10.6818 8.80682C11.25 8.46591 11.8778 8.29545 12.5653 8.29545C13.2585 8.29545 13.8864 8.46591 14.4489 8.80682C15.017 9.14205 15.4688 9.59375 15.804 10.1619C16.1449 10.7244 16.3153 11.3523 16.3153 12.0455C16.3153 12.733 16.1449 13.3608 15.804 13.929C15.4688 14.4972 15.017 14.9517 14.4489 15.2926C13.8864 15.6278 13.2585 15.7955 12.5653 15.7955Z" fill="currentColor"/>
+</svg>`, h = `<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_4_6)">
+<path d="M12.5739 16.1136C12.0578 16.1136 11.5724 16.0166 11.1179 15.8224C10.6657 15.6283 10.2668 15.3596 9.92116 15.0163C9.57789 14.6707 9.308 14.2718 9.11151 13.8196C8.91738 13.3674 8.8215 12.8821 8.82386 12.3636C8.82623 11.8452 8.92448 11.3598 9.11861 10.9077C9.3151 10.4555 9.58499 10.0578 9.92827 9.71449C10.2715 9.36884 10.6693 9.09896 11.1214 8.90483C11.5736 8.7107 12.0578 8.61364 12.5739 8.61364C13.0923 8.61364 13.5777 8.7107 14.0298 8.90483C14.4844 9.09896 14.8821 9.36884 15.223 9.71449C15.5663 10.0578 15.835 10.4555 16.0291 10.9077C16.2232 11.3598 16.3215 11.8452 16.3239 12.3636C16.3262 12.8821 16.2304 13.3674 16.0362 13.8196C15.8421 14.2718 15.5734 14.6707 15.2301 15.0163C14.8868 15.3596 14.4879 15.6283 14.0334 15.8224C13.5788 16.0166 13.0923 16.1136 12.5739 16.1136ZM12.5739 14.9311C12.9313 14.9311 13.2652 14.8648 13.5753 14.7322C13.8878 14.5997 14.1612 14.4162 14.3956 14.1818C14.63 13.9451 14.8134 13.6728 14.946 13.3651C15.0786 13.0549 15.1437 12.7223 15.1413 12.3672C15.1413 12.0097 15.0739 11.6759 14.9389 11.3658C14.8063 11.0533 14.6229 10.7798 14.3885 10.5455C14.1541 10.3111 13.8819 10.1276 13.5717 9.99503C13.2616 9.86245 12.929 9.79616 12.5739 9.79616C12.2211 9.79616 11.8897 9.86245 11.5795 9.99503C11.2718 10.1276 10.9995 10.3123 10.7628 10.549C10.5284 10.7834 10.3438 11.0568 10.2088 11.3693C10.0739 11.6795 10.0064 12.0121 10.0064 12.3672C10.004 12.7199 10.0691 13.0514 10.2017 13.3615C10.3366 13.6693 10.5213 13.9415 10.7557 14.1783C10.9924 14.4126 11.2659 14.5973 11.576 14.7322C11.8861 14.8648 12.2188 14.9311 12.5739 14.9311Z" fill="currentColor"/>
+</g>
+<defs>
+<clipPath id="clip0_4_6">
+<rect width="24" height="24" fill="white" transform="translate(0.5)"/>
+</clipPath>
+</defs>
+</svg>`, d = `<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_4_9)">
+<path d="M8.9375 16V8.72727H16.2102V16H8.9375Z" fill="currentColor"/>
+</g>
+<defs>
+<clipPath id="clip0_4_9">
+<rect width="24" height="24" fill="white" transform="translate(0.5)"/>
+</clipPath>
+</defs>
+</svg>`, c = `<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_4_12)">
+<path d="M12.7273 14.1321L10.0426 16.071L11.1009 12.9176L8.40909 10.9716H11.7045L12.7273 7.81818L13.75 10.9716H17.0455L14.3537 12.9176L15.4119 16.071L12.7273 14.1321Z" fill="currentColor"/>
+</g>
+<defs>
+<clipPath id="clip0_4_12">
+<rect width="24" height="24" fill="white" transform="translate(0.5)"/>
+</clipPath>
+</defs>
+</svg>`, p = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_5_15)">
+<path d="M15.0043 10.8267V13.2273H8.37074V10.8267H15.0043Z" fill="currentColor"/>
+</g>
+<defs>
+<clipPath id="clip0_5_15">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+</svg>`, u = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_5_18)">
+<path d="M12.4766 16.4375L11.5703 15.5391L13.9492 13.1602H8.375V11.8398H13.9492L11.5703 9.46484L12.4766 8.5625L16.4141 12.5L12.4766 16.4375Z" fill="currentColor"/>
+</g>
+<defs>
+<clipPath id="clip0_5_18">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+</svg>`, g = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7 9.91718L11.8586 5.05858C11.9367 4.98048 12.0633 4.98048 12.1414 5.05858L17 9.91718" stroke="black" stroke-width="2" stroke-linecap="round"/>
+<path d="M17 14L12.1414 18.8586C12.0633 18.9367 11.9367 18.9367 11.8586 18.8586L7 14" stroke="black" stroke-width="2" stroke-linecap="round"/>
+</svg>`;
+class w {
+  /**
+   * Notify core that read-only mode is supported
+   *
+   * @returns {boolean}
+   */
+  static get isReadOnlySupported() {
+    return !0;
+  }
+  /**
+   * Allow to use native Enter behaviour
+   *
+   * @returns {boolean}
+   * @public
+   */
+  static get enableLineBreaks() {
+    return !0;
+  }
+  /**
+   * Get Tool toolbox settings
+   * icon - Tool icon's SVG
+   * title - title to show in toolbox
+   *
+   * @returns {{icon: string, title: string}}
+   */
+  static get toolbox() {
+    return {
+      icon: a,
+      title: "List"
+    };
+  }
+  /**
+   * Render plugin`s main Element and fill it with saved data
+   *
+   * @param {object} params - tool constructor options
+   * @param {ListData} params.data - previously saved data
+   * @param {object} params.config - user config for Tool
+   * @param {object} params.api - Editor.js API
+   * @param {boolean} params.readOnly - read-only mode flag
+   */
+  constructor({ data: e, config: t, api: i, readOnly: n }) {
+    this._elements = {
+      wrapper: null
+    }, this.api = i, this.readOnly = n, this.settings = {
+      style: [
+        {
+          name: "numbered",
+          title: this.api.i18n.t("Numbered"),
+          icon: l,
+          default: t.defaultStyle === "numbered" || !0
+        },
+        {
+          name: "unordered",
+          title: this.api.i18n.t("Unordered"),
+          icon: o,
+          default: t.defaultStyle === "unordered" || !1
+        },
+        {
+          name: "circled",
+          title: this.api.i18n.t("Circled"),
+          icon: h,
+          default: t.defaultStyle === "circled" || !1
+        },
+        {
+          name: "squared",
+          title: this.api.i18n.t("Squared"),
+          icon: d,
+          default: t.defaultStyle === "squared" || !1
+        },
+        {
+          name: "stared",
+          title: this.api.i18n.t("Stared"),
+          icon: c,
+          default: t.defaultStyle === "stared" || !1
+        },
+        {
+          name: "dashed",
+          title: this.api.i18n.t("Dashed"),
+          icon: p,
+          default: t.defaultStyle === "dashed" || !1
+        },
+        {
+          name: "arrowed",
+          title: this.api.i18n.t("Arrowed"),
+          icon: u,
+          default: t.defaultStyle === "arrowed" || !1
+        }
+      ],
+      spacing: [
+        {
+          name: "normal",
+          title: this.api.i18n.t("Normal"),
+          icon: null,
+          default: t.defaultSpacing === "normal" || !0,
+          render: () => document.createElement("span")
+        },
+        {
+          name: "compact",
+          title: this.api.i18n.t("Compact"),
+          icon: null,
+          default: t.defaultSpacing === "compact" || !1
+        }
+      ]
+    }, this._data = {
+      style: this.settings.style.find((s) => s.default === !0).name,
+      spacing: this.settings.spacing.find((s) => s.default === !0).name,
+      items: []
+    }, this.data = e;
+  }
+  /**
+   * Returns list tag with items
+   *
+   * @returns {Element}
+   * @public
+   */
+  render() {
+    return this._elements.wrapper = this.makeMainTag(
+      this._data.style,
+      this._data.spacing
+    ), this._data.items.length ? this._data.items.forEach((e) => {
+      this._elements.wrapper.appendChild(this._make("li", this.CSS.item, {
+        innerHTML: e
+      }));
+    }) : this._elements.wrapper.appendChild(this._make("li", this.CSS.item)), this.readOnly || this._elements.wrapper.addEventListener("keydown", (e) => {
+      const [t, i] = [13, 8];
+      switch (e.keyCode) {
+        case t:
+          this.getOutofList(e);
+          break;
+        case i:
+          this.backspace(e);
+          break;
+      }
+    }, !1), this._elements.wrapper;
+  }
+  /**
+   * @returns {ListData}
+   * @public
+   */
+  save() {
+    return this.data;
+  }
+  /**
+   * Allow List Tool to be converted to/from other block
+   *
+   * @returns {{export: Function, import: Function}}
+   */
+  static get conversionConfig() {
+    return {
+      /**
+       * To create exported string from list, concatenate items by dot-symbol.
+       *
+       * @param {ListData} data - list data to create a string from thats
+       * @returns {string}
+       */
+      export: (e) => e.items.join(". "),
+      /**
+       * To create a list from other block's string, just put it at the first item
+       *
+       * @param {string} string - string to create list tool data from that
+       * @returns {ListData}
+       */
+      import: (e) => ({
+        items: [e],
+        style: "unordered"
+      })
+    };
+  }
+  /**
+   * Sanitizer rules
+   *
+   * @returns {object}
+   */
+  static get sanitize() {
+    return {
+      style: {},
+      items: {
+        span: !0,
+        br: !0,
+        a: !0,
+        i: !0,
+        b: !0,
+        s: !0
+      }
+    };
+  }
+  /**
+   * Settings
+   *
+   * @public
+   * @returns {Array}
+   */
+  renderSettings() {
+    const e = (t, i, n) => t.map((s) => ({
+      ...s,
+      closeOnActivate: !0,
+      isActive: i === s.name,
+      onActivate: () => n(s.name)
+    }));
+    return [
+      {
+        name: "style",
+        title: this.api.i18n.t("Style"),
+        icon: a,
+        children: {
+          items: e(
+            this.settings.style,
+            this._data.style,
+            (t) => this.toggleStyleTune(t)
+          )
+        }
+      },
+      {
+        name: "spacing",
+        title: this.api.i18n.t("Spacing"),
+        icon: g,
+        children: {
+          items: e(
+            this.settings.spacing,
+            this._data.spacing,
+            (t) => this.toggleSpacingTune(t)
+          )
+        }
+      }
+    ];
+  }
+  /**
+   * On paste callback that is fired from Editor
+   *
+   * @param {PasteEvent} event - event with pasted data
+   */
+  onPaste(e) {
+    const t = e.detail.data;
+    this.data = this.pasteHandler(t);
+  }
+  /**
+   * List Tool on paste configuration
+   *
+   * @public
+   */
+  static get pasteConfig() {
+    return {
+      tags: ["OL", "UL", "LI"]
+    };
+  }
+  /**
+   * Creates main <ul> or <ol> tag depended on style
+   *
+   * @param {string} style
+   * @param {string} spacing - 'compact' or 'normal'
+   * @returns {HTMLOListElement|HTMLUListElement}
+   */
+  makeMainTag(e, t) {
+    const i = e === "numbered" ? this.CSS.wrapperOrdered : `${this.CSS.wrapperList}${e}`, n = t === "compact" ? this.CSS.spacingCompact : this.CSS.spacingNormal, s = e === "numbered" ? "ol" : "ul";
+    return this._make(s, [
+      this.CSS.baseBlock,
+      this.CSS.wrapper,
+      i,
+      n
+    ], {
+      contentEditable: !this.readOnly
+    });
+  }
+  /**
+   * Toggles List style
+   *
+   * @param {string} style - 'ordered'|'unordered'
+   */
+  toggleStyleTune(e) {
+    const t = this.makeMainTag(
+      e,
+      this._data.spacing
+    );
+    for (; this._elements.wrapper.hasChildNodes(); )
+      t.appendChild(this._elements.wrapper.firstChild);
+    this._elements.wrapper.replaceWith(t), this._elements.wrapper = t, this._data.style = e;
+  }
+  /**
+   * Toggles List style
+   *
+   * @param {string} spacing - 'compact'|'normal'
+   */
+  toggleSpacingTune(e) {
+    const t = this.makeMainTag(this._data.style, e);
+    for (; this._elements.wrapper.hasChildNodes(); )
+      t.appendChild(this._elements.wrapper.firstChild);
+    this._elements.wrapper.replaceWith(t), this._elements.wrapper = t, this._data.spacing = e;
+  }
+  /**
+   * Styles
+   *
+   * @private
+   */
+  get CSS() {
+    return {
+      baseBlock: this.api.styles.block,
+      wrapper: "cdx-list",
+      wrapperOrdered: "cdx-list--ordered",
+      wrapperList: "cdx-list--",
+      spacingNormal: "cdx-list--normal",
+      spacingCompact: "cdx-list--compact",
+      item: "cdx-list__item"
+    };
+  }
+  /**
+   * List data setter
+   *
+   * @param {ListData} listData
+   */
+  set data(e) {
+    e || (e = {}), this._data.style = e.style || this.settings.style.find((i) => i.default === !0).name, this._data.spacing = e.spacing || this.settings.spacing.find((i) => i.default === !0).name, this._data.items = e.items || [];
+    const t = this._elements.wrapper;
+    t && t.parentNode.replaceChild(this.render(), t);
+  }
+  /**
+   * Return List data
+   *
+   * @returns {ListData}
+   */
+  get data() {
+    this._data.items = [];
+    const e = this._elements.wrapper.querySelectorAll(`.${this.CSS.item}`);
+    for (let t = 0; t < e.length; t++)
+      e[t].innerHTML.replace("<br>", " ").trim() && this._data.items.push(e[t].innerHTML);
+    return this._data;
+  }
+  /**
+   * Helper for making Elements with attributes
+   *
+   * @param  {string} tagName           - new Element tag name
+   * @param  {Array|string} classNames  - list or name of CSS classname(s)
+   * @param  {object} attributes        - any attributes
+   * @returns {Element}
+   */
+  _make(e, t = null, i = {}) {
+    const n = document.createElement(e);
+    Array.isArray(t) ? n.classList.add(...t) : t && n.classList.add(t);
+    for (const s in i)
+      n[s] = i[s];
+    return n;
+  }
+  /**
+   * Returns current List item by the caret position
+   *
+   * @returns {Element}
+   */
+  get currentItem() {
+    let e = window.getSelection().anchorNode;
+    return e.nodeType !== Node.ELEMENT_NODE && (e = e.parentNode), e.closest(`.${this.CSS.item}`);
+  }
+  /**
+   * Get out from List Tool
+   * by Enter on the empty last item
+   *
+   * @param {KeyboardEvent} event
+   */
+  getOutofList(e) {
+    const t = this._elements.wrapper.querySelectorAll("." + this.CSS.item);
+    if (t.length < 2)
+      return;
+    const i = t[t.length - 1], n = this.currentItem;
+    n === i && !i.textContent.trim().length && (n.parentElement.removeChild(n), this.api.blocks.insert(), this.api.caret.setToBlock(this.api.blocks.getCurrentBlockIndex()), e.preventDefault(), e.stopPropagation());
+  }
+  /**
+   * Handle backspace
+   *
+   * @param {KeyboardEvent} event
+   */
+  backspace(e) {
+    const t = this._elements.wrapper.querySelectorAll("." + this.CSS.item), i = t[0];
+    i && t.length < 2 && !i.innerHTML.replace("<br>", " ").trim() && e.preventDefault();
+  }
+  /**
+   * Select LI content by CMD+A
+   *
+   * @param {KeyboardEvent} event
+   */
+  selectItem(e) {
+    e.preventDefault();
+    const t = window.getSelection(), i = t.anchorNode.parentNode, n = i.closest("." + this.CSS.item), s = new Range();
+    s.selectNodeContents(n), t.removeAllRanges(), t.addRange(s);
+  }
+  /**
+   * Handle UL, OL and LI tags paste and returns List data
+   *
+   * @param {HTMLUListElement|HTMLOListElement|HTMLLIElement} element
+   * @returns {ListData}
+   */
+  pasteHandler(e) {
+    const { tagName: t } = e;
+    let i;
+    switch (t) {
+      case "OL":
+        i = "ordered";
+        break;
+      case "UL":
+      case "LI":
+        i = "unordered";
+    }
+    const n = {
+      style: i,
+      items: []
+    };
+    if (t === "LI")
+      n.items = [e.innerHTML];
+    else {
+      const s = Array.from(e.querySelectorAll("LI"));
+      n.items = s.map((r) => r.innerHTML).filter((r) => !!r.trim());
+    }
+    return n;
+  }
+}
+export {
+  w as default
+};
